8949f14f5d017568184d8731c82e4549752f6292
R9OEI <r9oei@gmail.com>
2024-08-22 12:18:00 +0700

    added Test merge

--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -1,2 +1,7 @@
-package com.company;public class Test {
+package com.company;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test merge");
+    }
 }
